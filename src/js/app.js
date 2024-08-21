beb383ca435111de10da23736e2699b25cb0458f
MohammadMehdis <your-email@example.com>
2024-08-21 16:35:45 +0330

    Update the code

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
-"use strict";
+"use strict"
 const userData = {
   email: "eve.holt@reqres.in",
   password: "cityslicka",
 };
-console.log(userData.email, userData.password);
+console.log(userData.email,userData.password);
 
 function showToast(message, isLog) {
   const toast = document.getElementById("toast");
@@ -11,14 +11,14 @@ function showToast(message, isLog) {
   toast.className = "toast show " + (isLog ? "" : "failure");
   setTimeout(() => {
     toast.className = "toast";
-  }, 3000);
+  }, 5000);
 }
 
 function loginUser(email, password) {
   if (email === userData.email && password === userData.password) {
-    showToast(`Welcome Back ${email}`, true);
+    showToast(`Welcome Back ${email}`,true);
   } else {
-    showToast(`Unknown login warning`, false);
+    showToast(`Unknown login warning undefind`,false);
   }
 }
 
